1aa0b7e1ab93e91125125b886b0657b11ba02838
Vikas Sangwan <69621488+VikasSangwan21@users.noreply.github.com>
2023-04-22 13:06:40 +0800

    added reporting

--- ui-tests/src/test/java/com/lynx/base/BaseTest.java
+++ ui-tests/src/test/java/com/lynx/base/BaseTest.java
@@ -1,20 +1,50 @@
 package com.lynx.base;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 import helper.DriverSetup;
 import utilities.PropertiesReader;
 
 public class BaseTest {
 	
-	
+	public ExtentReports extent = null;
 	
 	@BeforeClass
 	public void setup() {
 		System.out.println("Before Class execution");
 		PropertiesReader.initialiseUiProperties();
 		System.out.println(PropertiesReader.property.getProperty("implicitWaitTime"));
+		
+		// directory where output is to be printed
+		ExtentSparkReporter spark = new ExtentSparkReporter("C:\\Users\\User\\Desktop\\Automation\\Automation-testng\\ui-tests\\reports\\TestResults.html");
+		extent = new ExtentReports();
+		extent.attachReporter(spark);
+
+	}
+	
+	@BeforeTest
+	public void setupTest() {
+		System.out.println("Before Test execution");
+		ExtentSparkReporter spark = new ExtentSparkReporter("C:\\Users\\User\\Desktop\\Automation\\Automation-testng\\ui-tests\\reports\\TestResults.html");
+		extent = new ExtentReports();
+		extent.attachReporter(spark);
+
+		ExtentTest report = extent.createTest("New Test");
+
+	}
+	
+	@AfterTest
+	public void teardownTest() {
+		System.out.println("After Test execution");
+		extent.flush();
+
 	}
 	
 	@AfterClass
